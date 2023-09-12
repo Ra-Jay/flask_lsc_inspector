02d40feab8883fae69bb5f691b8d3da9011c1c54
kyr8er <labajokerr5@gmail.com>
2023-09-12 21:17:18 +0800

    Remove datetime usage and add another TODO comments

--- src/helpers/utility.py
+++ src/helpers/utility.py
@@ -1,22 +1,18 @@
 import os
 from ultralytics import YOLO
-from datetime import datetime
 from src.controllers.weights import get_weight
 
 def demo_analyze_image(filename):
     model = YOLO('src\\pre-trained_weights\\yolov8s\\lsc_v1.pt')
     
-    #TODO: Get the file path of the image from the filename parameter instead of predefining the address of the file.
+    # TODO: Get the file path of the image from the filename parameter instead of predefining the address of the file.
+    # (May need how the NextJS app will send the file to the backend)
     file_path = os.path.join('src\\uploads\\', filename)
-    
-    now = datetime.now()
-    date_time_str = now.strftime('%Y-%m-%d_%H-%M')
     
     result = model.predict(
         source=file_path, 
         show=False, 
         conf=0.20,
-        name=date_time_str,
         save=False
     )
     
@@ -24,22 +20,22 @@ def demo_analyze_image(filename):
   
 def custom_analyze_image(filename):
   
-    # TODO: Get the id of the selected custom weights of the user.
+    # TODO: Get the id of the selected/uploaded custom weights of the user. 
+    # (This is the id of the custom weights that was returned from handle_weights function, 
+    # so it may need to store the weight_id in the session)
     custom_weights = get_weight(id)
+    
     # TODO: Download custom weights from the url attribute of the custom_weights object to use as parameter for the YOLO object.
     model = YOLO('src\\pre-trained_weights\\yolov8s\\lsc_v1.pt')
     
-    #TODO: Get the file path of the image from the filename parameter instead of predefining the address of the file.
+    # TODO: Get the file path of the image from the filename parameter instead of predefining the address of the file.
+    # (May need how the NextJS app will send the file to the backend)
     file_path = os.path.join('src\\uploads\\', filename)
-    
-    now = datetime.now()
-    date_time_str = now.strftime('%Y-%m-%d_%H-%M')
     
     result = model.predict(
         source=file_path, 
         show=False, 
         conf=0.20,
-        name=date_time_str,
         save=False
     )
     
