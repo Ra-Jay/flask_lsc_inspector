5130f0a0568a88a300ae0ecba6487f0ce0ab22da
kyr8er <labajokerr5@gmail.com>
2023-09-12 10:39:41 +0800

    Synch weights.py and Weights table from ERD

--- src/models/weights.py
+++ src/models/weights.py
@@ -4,8 +4,10 @@ from datetime import datetime
 
 class Weights(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id'))
     name = db.Column(db.String(50), unique=True, nullable=False)
     url=db.Column(db.Text, nullable=False)
+    trained_at=db.Column(db.DateTime, nullable=True)
     created_at=db.Column(db.DateTime, default=datetime.now())
     updated_at=db.Column(db.DateTime, onupdate=datetime.now())
 
