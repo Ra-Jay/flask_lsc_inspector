4e9818667d50aef19d2cc340dd026707fcb319df
kyr8er <labajokerr5@gmail.com>
2023-09-12 20:59:51 +0800

    Separate demo and custom analyze_image methods with TODO comments

--- src/helpers/utility.py
+++ src/helpers/utility.py
@@ -1,12 +1,35 @@
 import os
 from ultralytics import YOLO
 from datetime import datetime
+from src.controllers.weights import get_weight
 
-def analyze_image(filename):
-    # If available na ang custom weights upload [FEATURE], 
-    # use the filepath/filename to load the custom weights instead.
+def demo_analyze_image(filename):
     model = YOLO('src\\pre-trained_weights\\yolov8s\\lsc_v1.pt')
     
+    #TODO: Get the file path of the image from the filename parameter instead of predefining the address of the file.
+    file_path = os.path.join('src\\uploads\\', filename)
+    
+    now = datetime.now()
+    date_time_str = now.strftime('%Y-%m-%d_%H-%M')
+    
+    result = model.predict(
+        source=file_path, 
+        show=False, 
+        conf=0.20,
+        name=date_time_str,
+        save=False
+    )
+    
+    return result
+  
+def custom_analyze_image(filename):
+  
+    # TODO: Get the id of the selected custom weights of the user.
+    custom_weights = get_weight(id)
+    # TODO: Download custom weights from the url attribute of the custom_weights object to use as parameter for the YOLO object.
+    model = YOLO('src\\pre-trained_weights\\yolov8s\\lsc_v1.pt')
+    
+    #TODO: Get the file path of the image from the filename parameter instead of predefining the address of the file.
     file_path = os.path.join('src\\uploads\\', filename)
     
     now = datetime.now()
