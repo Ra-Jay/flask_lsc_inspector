44259edc49b7e2deb21293edbe9939fe128e6181
kyr8er <labajokerr5@gmail.com>
2023-09-13 18:00:28 +0800

    Modify utility.py
    Modifications:
    1. Remove unused imports.
    2. Remove DISCONTINUED TODO.
    3. Add second parameter for custom_analyze_image method.

--- src/helpers/utility.py
+++ src/helpers/utility.py
@@ -1,6 +1,5 @@
 import os
 from ultralytics import YOLO
-from src.controllers.weights import get_weight
 
 # For demo purposes only
 DEMO_WEIGHTS_FOLDER = os.path.join('src', 'static', 'pre-trained_weights', 'yolov8s', 'lsc_v1.pt')
@@ -11,8 +10,6 @@ UPLOADS_FOLDER = os.path.join('src', 'static', 'uploads')
 def demo_analyze_image(filename):
     model = YOLO(DEMO_WEIGHTS_FOLDER)
     
-    # TODO: Get the file path of the image from the filename parameter instead of predefining the address of the file. [DISCONTINUED]
-    # (May need how the NextJS app will send the file to the backend) [DISCONTINUED]
     file_path = os.path.join(UPLOADS_FOLDER, filename)
     
     result = model.predict(
@@ -24,18 +21,12 @@ def demo_analyze_image(filename):
     
     return result
   
-def custom_analyze_image(filename):
-  
-    # TODO: Get the id of the selected/uploaded custom weights of the user. 
-    # (This is the id of the custom weights that was returned from handle_weights function, 
-    # so it may need to store the weight_id in the session)
-    custom_weights = get_weight(id)
+# Using the custom weights of the user that was retreived from the database through their user_id.
+def custom_analyze_image(filename, custom_weights):
     
-    # TODO: Download custom weights from the url attribute of the custom_weights object to use as parameter for the YOLO object.
+    # TODO: Download custom weights from the url attribute of the custom_weights object to use as parameter for the YOLO object. [RESEARCHING]
     model = YOLO(DEMO_WEIGHTS_FOLDER)
     
-    # TODO: Get the file path of the image from the filename parameter instead of predefining the address of the file. [DISCONTINUED]
-    # (May need how the NextJS app will send the file to the backend) [DISCONTINUED]
     file_path = os.path.join(UPLOADS_FOLDER, filename)
     
     result = model.predict(
