f5cb9f63a5082371f2fc48359cd240e265c32a03
kyr8er <labajokerr5@gmail.com>
2023-09-14 12:40:36 +0800

    Continue analyze_image method
    Modifications:
    1. Import torch and custom_analyze_image method from utility.py.
    2. Modify session variable name to uploaded_file.
    3. Implement existing_file condition and reuse to preview if it exists.
    4. Otherwise, use uploaded custom weights from session to pass as parameter when analyzing the uploaded file.
    5. Add TODO Tasks.
    6. Retrieve orig_img, cls, accuracy, and error rate.
    7. Create Files class.
    8. Add and commit file to db session.
    9. Return only selected details jsonified.

--- src/controllers/files.py
+++ src/controllers/files.py
@@ -1,9 +1,12 @@
 import os
+
+import torch
 from flask_restx import Resource, Namespace 
 from src.constants.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT, HTTP_404_NOT_FOUND
 from flask import Blueprint, request, jsonify
 from werkzeug.utils import secure_filename
-import validators  
+import validators
+from src.helpers.utility import custom_analyze_image  
 from src.models.files import Files
 from ..extensions import db
 from flask import session
@@ -40,7 +43,7 @@ def upload_file():
     path = os.path.join(UPLOADS_FOLDER, uploaded_filename)
       
     uploaded_file.save(path)
-    session['file_path'] = path
+    session['uploaded_file'] = path
     
     elapsed_time = time() - start_time
     print("===============================")
@@ -54,7 +57,69 @@ def analyze_file():
   current_user = get_jwt_identity()
   
   if request.method == 'POST':
-    uploaded_filepath = session.get('file_path')
+    uploaded_filepath = session.get('uploaded_filepath')
     
     if uploaded_filepath is None:
       return jsonify({'error': 'No uploaded file found.'}), HTTP_400_BAD_REQUEST
+    
+    uploaded_filename = os.path.basename(uploaded_filepath)
+    
+    existing_file = Files.query.filter_by(name=uploaded_filename, user_id=current_user).first()
+    
+    if existing_file:
+      # TODO: Download/Preview the existing file through its URL and return it.
+      # (Assuming the file can be previewed using its URL)
+      return jsonify({
+        'error': 'File already exists.',
+        'file_path': existing_file.url
+        }), HTTP_409_CONFLICT
+    
+    # Assuming the weights controller has also stored the custom weights in the session.
+    session_custom_weights = session.get('uploaded_custom_weights')
+    
+    print("===============================")
+    print("Analyzing " + uploaded_filename + " using the uploaded " + session_custom_weights['name'] + " weights")
+    start_time = time()
+    
+    # Predict the uploaded file using the custom weights of the user that was uploaded in the database and stored in session.
+    result = custom_analyze_image(uploaded_filename, session_custom_weights)
+    
+    elapsed_time = time() - start_time
+    print("\n===============================")
+    print(f"Image analyzed in {elapsed_time:.2f} seconds")
+    
+    # orig_img is of type numpy.ndarray
+    resulting_image = result.orig_img
+    
+    classification = "No Good" if torch.tensor(result[0].boxes.cls).item() > 0 else "Good"
+    accuracy = round(torch.tensor(result[0].boxes.conf).item(),2 )
+    error_rate = round(1 - accuracy, 2)
+    
+    # TODO: Get the dimensions and size of the uploaded file to be reused. Can also get the dimensions and size of the resulting image instead.
+    
+    # TODO: Generate URL to upload the resulting image to the cloud.
+    
+    file = Files(
+        name=uploaded_filename, 
+        url="TO BE IMPLEMENTED", 
+        user_id=current_user, 
+        classification=classification, 
+        accuracy=accuracy, 
+        error_rate=error_rate, 
+        dimensions="TO BE IMPLEMENTED", 
+        size="TO BE IMPLEMENTED"
+      )
+    db.session.add(file)
+    db.session.commit()
+    
+    return jsonify({
+      'id': file.id,
+      'name': file.name,
+      'dimensions': file.dimensions,
+      'size': file.size,
+      'url': file.url,
+      'classification': classification,
+      'accuracy': accuracy,
+      'error_rate': error_rate,
+      'resulting_image': resulting_image
+      }), HTTP_201_CREATED
