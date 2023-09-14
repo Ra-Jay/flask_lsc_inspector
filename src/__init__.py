65dba02bb5fadd9c198ef5fe452e1c9d3739eeaf
RJ <rjyansonx@gmail.com>
2023-09-14 20:10:31 +0800

    setup swagger docs

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-import os
+from src.constants.status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 from src.extensions import api, db
 from src.controllers.user import auth
 from src.controllers.weights import weights
 from flask_jwt_extended import JWTManager
+from flask_swagger_ui import get_swaggerui_blueprint
+import os
 
 def create_app(test_config=None):
      
@@ -15,8 +17,9 @@ def create_app(test_config=None):
             SECRET_KEY=os.environ.get('SECRET_KEY'),
             SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DB_URI'),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
-            JWT_SECRET_KEY=os.environ.get(  'JWT_SECRET_KEY')
+            JWT_SECRET_KEY=os.environ.get(  'JWT_SECRET_KEY'),
         )
+
 
     else: 
         app.config.from_mapping(test_config)
@@ -28,10 +31,29 @@ def create_app(test_config=None):
     # with app.app_context():
     #     db.create_all()
     
-    # api.add_namespace(ns)
 
     JWTManager(app)
     app.register_blueprint(auth)
     app.register_blueprint(weights)
+
+    SWAGGER_URL = '/swagger'
+    API_URL = '../static/swagger.json'
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,  
+        API_URL,
+        config={  
+            'app_name': "LSC-Inspector Application"
+        }
+    )
+
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
         
+    # @app.errorhandler(HTTP_404_NOT_FOUND)
+    # def handle_404(e):
+    #     return jsonify({'error': 'Not found'}), HTTP_404_NOT_FOUND
+
+    # @app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
+    # def handle_500(e):
+    #     return jsonify({'error': 'Something went wrong, we are working on it'}), HTTP_500_INTERNAL_SERVER_ERROR
+
     return app
