eae0406bc4aa75f3f9c64a004eefe4e052e9e16e
kyr8er <labajokerr5@gmail.com>
2023-09-14 13:59:47 +0800

    Update weights controller
    Modifications:
    1. Import os, time, session, and secure_filename.
    2. Declare WEIGHTS_FOLDER to store uploaded pre-trained weights by the user.
    3. Rename handle_weights to upload_weights.
    4. Implement file upload of the weights and local creation of WEIGHTS_FOLDER if doesn't exist.
    5. Store the weights in the folder.
    6. Add TODO Tasks.
    7. Rename filter queries using uploaded_weights_name.
    8. Fix incorrect attributes returned.

--- src/controllers/weights.py
+++ src/controllers/weights.py
@@ -1,6 +1,9 @@
+import os
+import time
 from flask_restx import Resource, Namespace 
 from src.constants.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT, HTTP_404_NOT_FOUND
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
+from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash, generate_password_hash
 import validators   
 from src.models.weights import Weights
@@ -10,14 +13,39 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 
 weights = Blueprint("weights", __name__, url_prefix="/api/v1/weights")
 
+WEIGHTS_FOLDER = os.path.join('src', 'static', 'pre-trained_weights')
+
 @weights.route('/', methods=['POST', 'GET'])
 @jwt_required()
-def handle_weights():
+def upload_weights():
     current_user = get_jwt_identity()
 
     if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({'error': 'No weights file found.'}), HTTP_400_BAD_REQUEST
+        
+        uploaded_weights = request.files['file']
+        
+        uploaded_weights_name = secure_filename(uploaded_weights.filename)
+        
+        print("===============================")
+        print("Uploading " + uploaded_weights_name)
+        start_time = time()
+
+        if not os.path.exists(WEIGHTS_FOLDER):
+            print("===============================")
+            print("Creating " + WEIGHTS_FOLDER + " folder")
+            os.makedirs(WEIGHTS_FOLDER)
+            
+        path = os.path.join(WEIGHTS_FOLDER, uploaded_weights_name)
+            
+        uploaded_weights.save(path)
+        session['uploaded_file'] = path
+            
         name = request.get_json().get('name', '')
         url = request.get_json().get('url', '')
+        
+        # TODO: Generate URL to upload the weights to the cloud.
 
         if not validators.url(url):
             return jsonify({'error': 'enter a valid url.'}), HTTP_400_BAD_REQUEST
@@ -25,17 +53,19 @@ def handle_weights():
         if Weights.query.filter_by(url=url).first():
             return jsonify({'error': 'URL already exists.'}), HTTP_409_CONFLICT
 
-        if Weights.query.filter_by(name=name).first():
+        if Weights.query.filter_by(name=uploaded_weights_name).first():
             return jsonify({'error': 'Name already exists.'}), HTTP_409_CONFLICT
 
-        weight = Weights(name=name, url=url, user_id=current_user)
+        weight = Weights(name=uploaded_weights_name, url=url, user_id=current_user)
         db.session.add(weight)
         db.session.commit()
-
+        
+        # TODO: Download/Preview the existing file through its URL and return it.
+        # (Assuming the file can be previewed using its URL)
         return jsonify({
             'id': weight.id,
             'name': weight.name,
-            'url': weight.name,
+            'url': weight.url,
             'created_at': weight.created_at,
             'udpated_at': weight.updated_at
         }), HTTP_201_CREATED
@@ -49,7 +79,7 @@ def handle_weights():
             data.append({
                 'id': weight.id,
                 'name': weight.name,
-                'ur': weight.url,
+                'url': weight.url,
                 'created_at': weight.created_at,
                 'updated_at': weight.updated_at
             })
