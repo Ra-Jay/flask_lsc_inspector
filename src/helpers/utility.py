1374ad95df5cd9d96789909c49031eb3af1a4e6a
kyr8er <labajokerr5@gmail.com>
2023-09-14 14:37:11 +0800

    Update utility.py
    Modifications:
    1. Add new importations.
    2. Add get_file_dimensions and get_file_size.
    3. File parameter is of type numpy.ndarray.
    4. Attempted conversion from np.ndarray to PIL Image.
    
    These methods are not tested yet.

--- src/helpers/utility.py
+++ src/helpers/utility.py
@@ -1,5 +1,8 @@
+import io
 import os
+import numpy as np
 from ultralytics import YOLO
+from PIL import Image
 
 # For demo purposes only
 DEMO_WEIGHTS_FOLDER = os.path.join('src', 'static', 'pre-trained_weights', 'yolov8s', 'lsc_v1.pt')
@@ -37,3 +40,24 @@ def custom_analyze_image(filename, custom_weights):
     )
     
     return result
+
+def get_file_dimensions(file):
+    try:
+        img = Image.fromarray(np.uint8(file))
+        width, height = img.size
+        return f"{width}x{height}"
+    except Exception as e:
+        print(f"Error while getting dimensions: {e}")
+        return None
+
+def get_file_size(file):
+    try:
+        img = Image.fromarray(np.uint8(file))
+        with io.BytesIO() as buf:
+            img.save(buf, format='PNG')
+            size_in_bytes = buf.tell()
+            size_in_kb = size_in_bytes / 1024
+            return f"{size_in_kb:.2f} kB"
+    except Exception as e:
+        print(f"Error while getting ndarray size: {e}")
+        return None
