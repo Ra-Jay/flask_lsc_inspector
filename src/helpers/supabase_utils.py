e437c4833c81925d40ee3d7464da25fa882a41a3
kyr8er <labajokerr5@gmail.com>
2023-09-19 10:01:33 +0800

    Update supabase_utils.py
    Modifications:
    1. Get SUPABASE_BUCKET from .flaskenv.
    2. Sync bucket variable as parameters for all functions.
    3. Update method names.
    4. Add get all files from bucket and delete file by name.

--- src/helpers/supabase_utils.py
+++ src/helpers/supabase_utils.py
@@ -3,19 +3,26 @@ from supabase import create_client, Client
 
 url = os.environ.get('SUPABASE_URL')
 key = os.environ.get('SUPABASE_KEY')
+bucket = os.environ.get('SUPABASE_BUCKET')
 
 supabase: Client = create_client(url, key, options={'timeout': 10})
 
-def supabase_upload(filepath):
+def upload_file_to_bucket(filepath):
     with open(filepath, 'rb+') as f:
-        response = supabase.storage.from_('lsc_bucket').upload(filepath, f)
+        response = supabase.storage.from_(bucket).upload(filepath, f)
         
     return response
 
-def supabase_download(filename):
-    data = supabase.storage.from_('lsc_bucket').download(filename)
+def download_file_from_bucket(filename):
+    data = supabase.storage.from_(bucket).download(filename)
     
     return data
 
-def supabase_get_url(filename):
-    return supabase.storage.from_('lsc_bucket').get_public_url(filename)
+def get_file_url_by_name(filename):
+    return supabase.storage.from_(bucket).get_public_url(filename)
+
+def get_all_files_from_bucket():
+    return supabase.storage.from_(bucket).list()
+
+def delete_file_by_name(filename):
+    return supabase.storage.from_(bucket).remove(filename)
