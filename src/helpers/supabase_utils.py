25f283b733edf177b84ec3d6181e35ab29e31201
kyr8er <labajokerr5@gmail.com>
2023-09-19 22:03:49 +0800

    Modify supabase_utils.py
    Modifications:
    1. Add get_bucket_type.
    2. Add bucket parameter for all existing functions.
    3. Get bucket type to be looked for.

--- src/helpers/supabase_utils.py
+++ src/helpers/supabase_utils.py
@@ -9,22 +9,32 @@ profile_images = os.environ.get('SUPABASE_BUCKET_PROFILE_IMAGES')
 
 supabase: Client = create_client(url, key)
 
-def upload_file_to_bucket(filepath):
+def get_bucket_type(bucket):
+    if bucket == 'files':
+        return files
+    elif bucket == 'weights':
+        return weights
+    elif bucket == 'profile_images':
+        return profile_images
+    else:
+        return None
+
+def upload_file_to_bucket(bucket, filepath):
     with open(filepath, 'rb+') as f:
-        response = supabase.storage.from_(bucket).upload(filepath, f)
+        response = supabase.storage.from_(get_bucket_type(bucket)).upload(filepath, f)
         
     return response
 
-def download_file_from_bucket(filename):
-    data = supabase.storage.from_(bucket).download(filename)
+def download_file_from_bucket(bucket, filename):
+    data = supabase.storage.from_(get_bucket_type(bucket)).download(filename)
     
     return data
 
-def get_file_url_by_name(filename):
-    return supabase.storage.from_(bucket).get_public_url(filename)
+def get_file_url_by_name(bucket, filename):
+    return supabase.storage.from_(get_bucket_type(bucket)).get_public_url(filename)
 
-def get_all_files_from_bucket():
-    return supabase.storage.from_(bucket).list()
+def get_all_files_from_bucket(bucket):
+    return supabase.storage.from_(get_bucket_type(bucket)).list()
 
-def delete_file_by_name(filename):
-    return supabase.storage.from_(bucket).remove(filename)
+def delete_file_by_name(bucket, filename):
+    return supabase.storage.from_(get_bucket_type(bucket)).remove(filename)
