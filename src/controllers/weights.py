e66fc6080bcedc780829ec97bac272b1812170e7
kyr8er <labajokerr5@gmail.com>
2023-09-20 16:48:55 +0800

    Implement supabase uploadation in upload_weights for weights controller

--- src/controllers/weights.py
+++ src/controllers/weights.py
@@ -1,10 +1,11 @@
 import os
 import time
 from flask_restx import Resource, Namespace 
-from src.constants.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT, HTTP_404_NOT_FOUND
+from src.constants.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 from flask import Blueprint, request, jsonify, session
 from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash, generate_password_hash
+from src.helpers.supabase_utils import upload_file_to_bucket, download_file_from_bucket, get_file_url_by_name, get_all_files_from_bucket, delete_file_by_name
 import validators   
 from src.models.weights import Weights
 from ..extensions import db
@@ -15,7 +16,7 @@ weights = Blueprint("weights", __name__, url_prefix="/api/v1/weights")
 
 WEIGHTS_FOLDER = os.path.join('src', 'static', 'pre-trained_weights')
 
-@weights.route('/', methods=['POST', 'GET'])
+@weights.route('/upload', methods=['POST', 'GET'])
 @jwt_required()
 def upload_weights():
     current_user = get_jwt_identity()
@@ -24,71 +25,50 @@ def upload_weights():
         if 'file' not in request.files:
             return jsonify({'error': 'No weights file found.'}), HTTP_400_BAD_REQUEST
         
-        uploaded_weights = request.files['file']
+        uploaded_weights = request.files['weights']
         
         uploaded_weights_name = secure_filename(uploaded_weights.filename)
         
         print("===============================")
-        print("Uploading " + uploaded_weights_name)
+        print("Uploading " + uploaded_weights_name + " weights to the supabase bucket")
         start_time = time()
-
-        if not os.path.exists(WEIGHTS_FOLDER):
-            print("===============================")
-            print("Creating " + WEIGHTS_FOLDER + " folder")
-            os.makedirs(WEIGHTS_FOLDER)
-            
-        path = os.path.join(WEIGHTS_FOLDER, uploaded_weights_name)
-            
-        uploaded_weights.save(path)
-        session['uploaded_file'] = path
         
-        elapsed_time = time() - start_time 
+        response = upload_file_to_bucket('weights', uploaded_weights)
+        
         print("===============================")
-        print(f"New weights saved locally at {path} folder in {elapsed_time:.2f} seconds")
+        elapsed_time = time() - start_time
+        
+        if response is HTTP_200_OK:
+            print(f"Successfully uploaded the {uploaded_weights_name} weights to the supabase bucket in {elapsed_time:.2f} seconds")
             
-        name = request.get_json().get('name', '')
-        url = request.get_json().get('url', '')
+            url = get_file_url_by_name(uploaded_weights_name)
+            
+            if url is None:
+                return jsonify({'error': 'Failed to get the url of the uploaded weights.'}), HTTP_404_NOT_FOUND
         
-        # TODO: Generate URL to upload the weights to the cloud.
+            session['uploaded_custom_weights_url'] = url
+            
+            weight = Weights(name=uploaded_weights_name, url=url, user_id=current_user)
+            db.session.add(weight)
+            db.session.commit()
 
-        if not validators.url(url):
-            return jsonify({'error': 'enter a valid url.'}), HTTP_400_BAD_REQUEST
-        
-        if Weights.query.filter_by(url=url).first():
-            return jsonify({'error': 'URL already exists.'}), HTTP_409_CONFLICT
-
-        if Weights.query.filter_by(name=uploaded_weights_name).first():
-            return jsonify({'error': 'Name already exists.'}), HTTP_409_CONFLICT
-
-        weight = Weights(name=uploaded_weights_name, url=url, user_id=current_user)
-        db.session.add(weight)
-        db.session.commit()
-        
-        # TODO: Download/Preview the existing file through its URL and return it.
-        # (Assuming the file can be previewed using its URL)
-        return jsonify({
-            'id': weight.id,
-            'name': weight.name,
-            'url': weight.url,
-            'created_at': weight.created_at,
-            'udpated_at': weight.updated_at
-        }), HTTP_201_CREATED
-
-    else:
-        weights = Weights.query.filter_by(user_id=current_user)
-
-        data=[]
-
-        for weight in weights:
-            data.append({
+            return jsonify({
                 'id': weight.id,
                 'name': weight.name,
                 'url': weight.url,
                 'created_at': weight.created_at,
-                'updated_at': weight.updated_at
-            })
-
-        return jsonify({'data': data}), HTTP_200_OK
+                'udpated_at': weight.updated_at
+            }), HTTP_201_CREATED
+            
+        elif response is HTTP_400_BAD_REQUEST:
+            print("The weights " + uploaded_weights_name + " was not found. Failed to upload to the supabase bucket")
+            return jsonify({'error': "The weights " + uploaded_weights_name + " was not found. Failed upload to the supabase bucket"}), HTTP_400_BAD_REQUEST
+        elif response is HTTP_409_CONFLICT:
+            print("The weights " + uploaded_weights_name + " already exists in the supabase bucket")
+            return jsonify({'error': "The weights " + uploaded_weights_name + " already exists in the supabase bucket"}), HTTP_409_CONFLICT
+        else:
+            print("Internal server error either in supabase or weights controller.")
+            return jsonify({'error': "Internal server error either in supabase or weights controller."}), HTTP_500_INTERNAL_SERVER_ERROR
 
 
 @weights.get('/<int:id>')
