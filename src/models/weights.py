4ea44c4ffaeae4679e2cb0555c8ca3aae0dfd565
kerrlabajo <labajokerr5@gmail.com>
2023-10-16 11:30:17 +0800

    Update weights controller and model
    Modifcations:
    1. Add type handler for weights uploadation.
    2. Set to nullable workspace and model type. Add type column for weights model.

--- src/models/weights.py
+++ src/models/weights.py
@@ -5,13 +5,14 @@ from datetime import datetime
 class Weights(db.Model):
     id = db.Column(db.String(50), primary_key=True)
     user_id=db.Column(db.String(50), db.ForeignKey('users.id'))
-    workspace=db.Column(db.String(50), nullable=False)
+    workspace=db.Column(db.String(50), nullable=True)
     project_name = db.Column(db.String(50), nullable=False)
     api_key=db.Column(db.String(50),unique=True, nullable=False)
     version=db.Column(db.Integer, nullable=True)
-    model_type=db.Column(db.String(50), nullable=False)
+    model_type=db.Column(db.String(50), nullable=True)
     created_at=db.Column(db.DateTime, default=datetime.now())
     updated_at=db.Column(db.DateTime, onupdate=datetime.now())
+    type=db.Column(db.String(50), nullable=False)
 
     # user = db.relationship('Users', backref='weights')
 
