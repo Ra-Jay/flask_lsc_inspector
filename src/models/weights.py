6532bdbe4004ba5597a439547bbc0eced534a157
kerrlabajo <labajokerr5@gmail.com>
2023-10-26 20:23:31 +0800

    Remove comments for all models

--- src/models/weights.py
+++ src/models/weights.py
@@ -1,7 +1,6 @@
 from ..extensions import db
 from datetime import datetime
 
-
 class Weights(db.Model):
     id = db.Column(db.String(50), primary_key=True)
     user_id=db.Column(db.String(50), db.ForeignKey('users.id'))
@@ -13,9 +12,4 @@ class Weights(db.Model):
     created_at=db.Column(db.DateTime, default=datetime.now())
     updated_at=db.Column(db.DateTime, onupdate=datetime.now())
     type=db.Column(db.String(50), nullable=False)
-
-    # user = db.relationship('Users', backref='weights')
-
-    # def __repr__(self) -> str:
-    #     return `Weights>> {self.id}`
         
