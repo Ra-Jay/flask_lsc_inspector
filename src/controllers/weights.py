8dbf50e963f03312eab07fc38d560f036d15045d
kerrlabajo <labajokerr5@gmail.com>
2024-02-12 02:53:56 +0800

    Add delete file by name

--- src/controllers/weights.py
+++ src/controllers/weights.py
@@ -1,3 +1,4 @@
+from helpers.supabase_utils import delete_file_by_name
 from src.models.files import Files
 from src.constants.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_409_CONFLICT, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 from flask import Blueprint, request, jsonify
